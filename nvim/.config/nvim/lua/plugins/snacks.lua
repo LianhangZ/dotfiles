aa61575d16d3ba66cb0c55f80359c2bda2d05065
LianhangZ <lz481456@gmail.com>
2026-03-13 19:08:01 +1100

    add `nvim`
    show hidden dotfiles in snacks' explorer
    fix end of line

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,27 @@
+-- ~/.config/nvim/lua/plugins/snacks.lua
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        position = "right",
+        width = 0.25,
+      },
+    },
+    explorer = {
+      enabled = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          follow_file = true,
+          layout = {
+            preset = "sidebar",
+            preview = false,
+          }
+        }
+      }
+    }
+  },
+}
