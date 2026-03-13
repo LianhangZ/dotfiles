895a49c496819333dbd0597901ea4da1c17b9620
LianhangZ <lz481456@gmail.com>
2026-03-13 19:08:01 +1100

    add `nvim`

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/lua/plugins/snacks.lua
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        position = "right",
+        width = 0.25,
+      },
+    },
+  },
+}
