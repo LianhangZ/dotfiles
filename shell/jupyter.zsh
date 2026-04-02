7f7e5b1bf518b1e32dab223727cb51ae47267fac
LianhangZ <lz481456@gmail.com>
2026-04-03 04:58:45 +1100

    `jupyter`: add `deregister-venv`

--- shell/jupyter.zsh
+++ shell/jupyter.zsh
@@ -13,6 +13,23 @@ delete_venv() {
     # TO-DO: delete ~/Library/Jupyter/env-name
 }
 
+deregister-venv() {
+    local base_dir="$HOME/Library/Jupyter/kernels/"
+    if [[ -z "$1" ]]; then
+        cd "$base_dir" || return 1
+    else
+        # cd "$base_dir/$1" || return 1
+        rm -rf "$base_dir/$1" || return 1   # ~/Library/Jupyter
+    fi
+    # echo "deregister"
+}
+
+_deregister-venv_completion() {
+  _files -W "$HOME/Library/Jupyter/kernels/" -/
+}
+
+compdef _deregister-venv_completion deregister-venv
+
 show_venv() {
     ls ~/Library/Jupyter/kernels
 }
