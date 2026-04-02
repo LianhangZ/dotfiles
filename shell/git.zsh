de34ae433858889e1bb044ff1ff53d5f85cbd278
LianhangZ <lz481456@gmail.com>
2026-04-03 01:17:21 +1100

    `git.zsh`: command hint

--- shell/git.zsh
+++ shell/git.zsh
@@ -1,3 +1,13 @@
 alias status='git status -sb' # /opt/homebrew/bin/gs
 alias stash='git stash -ku'
 alias pop='git stash pop'
+
+# printf "user.name: "
+# git config user.name
+# printf "user.email: "
+# git config user.email
+
+# git config --global user.name "name"
+# git config --global user.email "email"
+
+# git config --global --list
