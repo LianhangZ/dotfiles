1d6a1279947daf8b38f2103666b4fddf3dd3552b
Lianhang Zhang <148300543+LianhangZ@users.noreply.github.com>
2026-05-18 04:56:46 +1000

    "sync" fix

--- ubuntu/sync.sh
+++ ubuntu/sync.sh
@@ -1,9 +1,9 @@
-unlink ~/.zshrc
-unlink ~/.zshenv
+rm -f ~/.zshrc
+rm -f ~/.zshenv
 ln -sf ~/dotfiles/ubuntu/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/ubuntu/zsh/zshenv ~/.zshenv
 
-unlink ~/.config/nvim/lazy-lock.json
+rm -f ~/.config/nvim/lazy-lock.json
 ln -sf ~/dotfiles/ubuntu/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 
 bash ~/dotfiles/shared/link.sh
