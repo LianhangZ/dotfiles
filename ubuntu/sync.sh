df97b0c4caacf1348eee0ff3678d05b562db7d71
Lianhang Zhang <148300543+LianhangZ@users.noreply.github.com>
2026-05-18 02:22:34 +1000

    `sync.sh`

--- ubuntu/sync.sh
+++ ubuntu/sync.sh
@@ -11,6 +11,23 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
+install_font() {
+    mkdir -p ~/.local/share/fonts
+    cd ~/.local/share/fonts
+
+    wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+
+    sudo apt install -y unzip
+    unzip JetBrainsMono.zip -d JetBrainsMono
+    fc-cache -fv
+}
+
+install_font
+
+
+ln -sf ~/dotfiles/ubuntu/zshrc ~/.zshrc
+
 unlink ~/.config/nvim/lazy-lock.json
 ln -sf ~/dotfiles/ubuntu/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 
