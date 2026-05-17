9700bd9711a225ee94db837c011a96169a7186ca
Lianhang Zhang <148300543+LianhangZ@users.noreply.github.com>
2026-05-18 05:04:49 +1000

    `ubuntu` add `p10k`

--- ubuntu/sync.sh
+++ ubuntu/sync.sh
@@ -6,4 +6,7 @@ ln -sf ~/dotfiles/ubuntu/zsh/zshenv ~/.zshenv
 rm -f ~/.config/nvim/lazy-lock.json
 ln -sf ~/dotfiles/ubuntu/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 
+rm -f ~/.p10k.zsh
+ln -sf ~/dotfiles/ubuntu/zsh/p10k.zsh ~/.p10k.zsh
+
 bash ~/dotfiles/shared/link.sh
