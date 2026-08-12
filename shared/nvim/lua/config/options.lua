b6260ead314beb82b8b7d434f441f412e5f1c402
Lianhang Zhang <148300543+LianhangZ@users.noreply.github.com>
2026-08-13 05:17:11 +1000

    modify options

--- shared/nvim/lua/config/options.lua
+++ shared/nvim/lua/config/options.lua
@@ -12,6 +12,44 @@ vim.g.autoformat = false
 
 opt.fixeol = false  -- fix end of line
 
+opt.termguicolors = true
+
+opt.number = true
+opt.relativenumber = false
+opt.cursorline = true
+opt.cursorlineopt = "number,line"
+opt.signcolumn = "yes"
+opt.foldcolumn = "0"
+
+opt.laststatus = 3
+opt.showtabline = 2
+opt.showmode = false
+opt.cmdheight = 1
+opt.ruler = false
+
+opt.splitright = true
+opt.splitbelow = true
+opt.equalalways = false
+
+opt.winblend = 0
+opt.pumblend = 0
+opt.winborder = "rounded"
+
+opt.fillchars = {
+  eob = " ",
+  fold = " ",
+  foldopen = "",
+  foldsep = " ",
+  foldclose = "",
+  horiz = "─",
+  horizdown = "┬",
+  horizup = "┴",
+  vert = "│",
+  vertleft = "┤",
+  vertright = "├",
+  verthoriz = "┼",
+}
+
 vim.opt.guicursor = {
   "n-v-c:block-blinkwait500-blinkon500-blinkoff500",
   "i-ci-ve:ver25-blinkwait500-blinkon500-blinkoff500",
