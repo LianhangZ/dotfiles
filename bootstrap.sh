83a6aafd601822ec8c7401df76a6dcadc839b344
Lianhang Zhang <148300543+LianhangZ@users.noreply.github.com>
2026-09-01 03:01:19 +1000

    feature: bootstrap
    clean bootstrap
    link lazygit in bootstrap
    install brew in boot
    check install nvim, lazygit, zsh plugins, rg
    check install zsh in boot
    check install fd, aerospace and sketchybar

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+set -e; sudo -v || exit 1
+
+source ~/dotfiles/shared/function.sh
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  check_install brew
+  check_install fd
+  check_install aerospace
+  check_install sketchybar
+  link_zsh "$HOME/dotfiles/macos/zsh"
+elif [[ "$(uname -s)" == "Linux" ]]; then
+  source /etc/os-release
+
+  check_install zsh
+  link_zsh "$HOME/dotfiles/linux/shared/zsh"
+
+  # chsh -s $(which zsh)
+  if [ -n "$CI" ]; then
+    echo "CI detected, skip chsh"
+  else
+    chsh -s "$(command -v zsh)"
+  fi
+fi
+
+check_install nvim
+check_install lazygit
+check_install ohmyzsh
+check_install p10k
+check_install autosuggestions
+check_install syntax-highlighting
+check_install rg
+
+link_nvim
+link_lazygit
